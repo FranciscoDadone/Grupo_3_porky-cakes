59ecedb324e85c60775da1bba0193e1bc4723633
Francisco Dadone <dadonefran@gmail.com>
2025-09-16 19:35:37 -0300

    Usuario Data

--- src/main/java/com/ayds2/grupo3/Grupo3/models/Usuario.java
+++ src/main/java/com/ayds2/grupo3/Grupo3/models/Usuario.java
@@ -1,5 +1,8 @@
 package com.ayds2.grupo3.Grupo3.models;
 
+import lombok.Data;
+
+@Data
 public class Usuario {
     private int id;
     private String nombre;
@@ -10,38 +13,6 @@ public class Usuario {
         this.id = id;
         this.nombre = nombre;
         this.email = email;
-        this.imagen = imagen;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(String imagen) {
         this.imagen = imagen;
     }
 }
