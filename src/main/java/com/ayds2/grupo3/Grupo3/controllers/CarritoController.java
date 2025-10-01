899d872759763b40aa0fa47232cbf57c46f51075
Francisco Dadone <dadonefran@gmail.com>
2025-10-01 20:29:18 -0300

    Devolver error si falla

--- src/main/java/com/ayds2/grupo3/Grupo3/controllers/CarritoController.java
+++ src/main/java/com/ayds2/grupo3/Grupo3/controllers/CarritoController.java
@@ -1,9 +1,13 @@
 package com.ayds2.grupo3.Grupo3.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import com.ayds2.grupo3.Grupo3.services.CarritoService;
 import com.ayds2.grupo3.Grupo3.dto.AgregarProductoRequest;
 import lombok.AllArgsConstructor;
+import java.util.Map;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +19,13 @@ public class CarritoController {
     private final CarritoService carritoService;
 
     @PostMapping("/productos")
-    public void agregarProducto(@RequestBody AgregarProductoRequest request) {
-        carritoService.agregarProducto(request.getProductoId(), request.getCantidad(), request.getClienteId());
+    public ResponseEntity<Map<String, String>> agregarProducto(@RequestBody AgregarProductoRequest request) {
+        try {
+            carritoService.agregarProducto(request.getProductoId(), request.getCantidad(), request.getClienteId());
+        } catch (ResponseStatusException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Map.of("error", e.getReason()));
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("status", "Producto agregado al carrito"));
     }
 }
